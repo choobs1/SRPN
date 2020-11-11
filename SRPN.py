ac72cd96c7b5a4f2020366553f72f56e990f3aee
choobs1 <57880175+choobs1@users.noreply.github.com>
2020-11-11 20:27:44 +0000

    Update SRPN.py
    Added more functionality and documentation

--- SRPN.py
+++ SRPN.py
@@ -1,42 +1,39 @@
+"""
+@Author: Aman
+Note: Multiline strings are used in some places as comments and are not to be
+confused with program functionality.
+"""
+
+#list used as a stack data structure for the calculator
 stack = []
+
+# re -> regex used for token separation, random -> used to generate a random int
 import re
+import random as ran
 
 
+#The main function is responsible for the program running
 def main():
-    print("You can now interact with the SRPN calculator")
-    print()
-    
-    """while True:
-        print(stack)
-        token_list = []
-        #print("Give input:")
-        in_char = input()
-        if len(in_char) > 1:
-            token_list.extend(re.split("([^0-9])",in_char))
-        else:
-            if in_char in token_list:
-                return
-            else:
-                token_list.append(in_char)
-        print(token_list)
-        for token in token_list:
-            process_command(token)"""
+    print("You can now start interacting with the SRPN calculator")
     user_input()
 
 
+#The function is responsible for the calculations that happen
 def process_command(op):
-    if op == "" or op == " ":
+
+    #If the certain characters or 
+    if op == "" or op == " " or op == "#" or op.isalpha() == True and op != "d" and op != "r" :
         return
-    if len(stack) < 2 :
-        print("Stack underflow.")
-    """if op == "+":
+
+    # Check for the operator, if found, do the operation on the top two numbers on the stack
+    if op == "+":
         x = stack.pop()
         y = stack.pop()
         stack.append(x+y)
     elif op == "-":
         x = stack.pop()
         y = stack.pop()
-        stack.append(x-y)
+        stack.append(-(x-y))
     elif op == "*":
         x = stack.pop()
         y = stack.pop()
@@ -44,48 +41,58 @@ def process_command(op):
     elif op == "/":
         x = stack.pop()
         y = stack.pop()
-        if y == 0:
-            print("Divide by 0")
-        else:
-            stack.append(x/y)"""
-    perform_operation(op)
-    if op == "=":
+        #If division by zero occurs, kill stack and alert user
+        try:
+            stack.append(1/(x / y))
+        except ZeroDivisionError:
+            print("Divide by 0.")     
+    elif op == "^":
+        x = stack.pop()
+        y = stack.pop()
+        stack.append(x**y)
+    elif op == "%":
+        x = stack.pop()
+        y = stack.pop()
+        srack.append(x % y)
+    elif op == "=":
         print(stack[-1])
+
+    #Specific characters which affect the calculator
+        #r -> appends random int
+        #d -> prints the numbers in the stack
+    elif op == "r":
+        stack.append(ran.randint(0,2147483648))
     elif op == "d":
         print_stack(stack)
+
+    #if the token passed to the function is anything other than the defined values, append it to the stack
     else:
         stack.append(int(op))
 
+
+#Function prints all the items in the stack
 def print_stack(s):
     for i in s:
         print(i)
 
-def perform_operation(oper):
-    if oper == "+":
-        x = stack.pop()
-        y = stack.pop()
-        stack.append(x+y)
-    elif oper == "-":
-        x = stack.pop()
-        y = stack.pop()
-        stack.append(x-y)
-    elif oper == "*":
-        x = stack.pop()
-        y = stack.pop()
-        stack.append(x*y)
-    elif oper == "/":
-        x = stack.pop()
-        y = stack.pop()
-        if y == 0:
-            print("Divide by 0")
-        else:
-            stack.append(x/y)
-
+#Function takes in user input for the calculator
 def user_input():
+    
+    #While loop that runs continuosly for the user input
     while True:
-        print(stack)
+
+        """Functions used to check if the token_list and stack are working"""
+        #print(token_list)
+        #print(stack)
+
+        """A token list is used to separate numbers and chars in the input and then input the tokens
+        into the process_command() function"""
         token_list = []
-        #print("Give input:")
+
+        """Check the requirements to put the char in our token list:
+            the if statement checks if we get multiple inputs and the else statement
+            checks if the char is already present in the token list or not
+            """
         in_char = input()
         if len(in_char) > 1:
             token_list.extend(re.split("([^0-9])",in_char))
@@ -94,8 +101,12 @@ def user_input():
                 return
             else:
                 token_list.append(in_char)
-        print(token_list)
         for token in token_list:
-            process_command(token)
+            #Try and catch block checks if operation is being done on a single number
+            try:
+                process_command(token)
+            except IndexError:
+               print("Stack Underflow.")
 
+# Main function called to run the program
 main()
